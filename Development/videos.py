3cca93a68420e67122fc4956124ed537eec2c301
Joao-Pedro132 <amaraljoaopedroperboni@gmail.com>
2026-05-27 09:02:17 -0300

    video 1.1

---
+++ Development/videos.py
@@ -0,0 +1,11 @@
+print('=====Canais/Professores para estudos=====')
+sistema = True
+
+while sistema:
+    print('1-Continuar para ver canais de estudos\n2- Sair ')
+    escolha = int(input(': '))
+    if escolha == 1:
+        print('Escolha a matéria\n1 - Biologia\n2 - Filosofia\n3 - Sociologia\n4 - Física\n5 - Geografia\n6 - História\n7 - Inglês\n8 - Matemática\n9 - Português\n10 - Redação\n11 - Química')
+
+    else:
+        sistema = False
