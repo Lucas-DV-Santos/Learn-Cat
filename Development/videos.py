84c9459531a02d1a1e92f72ec8a127ada3697688
Joao-Pedro132 <amaraljoaopedroperboni@gmail.com>
2026-05-27 09:06:30 -0300

    videos 1.2

--- Development/videos.py
+++ Development/videos.py
@@ -6,6 +6,15 @@ while sistema:
     escolha = int(input(': '))
     if escolha == 1:
         print('Escolha a matéria\n1 - Biologia\n2 - Filosofia\n3 - Sociologia\n4 - Física\n5 - Geografia\n6 - História\n7 - Inglês\n8 - Matemática\n9 - Português\n10 - Redação\n11 - Química')
-
+        escolhaMateria = int(input(': '))
+        match escolhaMateria:
+            case 1:
+                print('')
+            case 2:
+                print('')
+            case 3:
+                print('')
+            case 4:
+                print('')
     else:
         sistema = False
