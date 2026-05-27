b407369f830b480f4e28a4be65028a44d5d2619c
Mateus-Tessaro <mateusdutra3777@gmail.com>
2026-05-27 08:57:50 -0300

    Menu pós Login

---
+++ Development/Menu.py
@@ -0,0 +1,4 @@
+print('----------MENU----------')
+print('Bem Vindo ao Learn Cat!!')
+print('===========😹===========')
+int(input("Por onde podemos começar?! \n [1] Descobrir Professores \n [2] Adicionar Questões \n [3] Chat com Learn Cat \n [4] Modelos de Estudos \n [5] Sair "))
